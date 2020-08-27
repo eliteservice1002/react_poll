53fde941b2c7e2b52eaf8a34e11b1cfe68a929ba
Alex10002 <nike199000@outlook.com>
2020-08-28 01:05:26 +0700

    poll challenge resolved.

--- components/Poll.tsx
+++ components/Poll.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { QandAsDocument } from '../types';
 
@@ -6,9 +6,120 @@ type Props = {
   qandas: QandAsDocument /* q and a's -- questions and answers document */;
 };
 
-const PollWrapper = styled.div``;
+const PollWrapper = styled.div`
+  padding: 20px;
+  border-radius: 5px;
+  background: white;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  width: 50%;
+  margin: auto;
+  & h2 {
+    margin-bottom: 20px;
+  }
+  & .answer {
+    padding: 10px;
+    border-radius: 7px;
+    background: white;
+    border: 1px solid #999;
+    display: flex;
+    justify-content: space-between;
+    position: relative;
+    margin-bottom: 10px;
+    & .portion {
+      position: absolute;
+      left: 0;
+      background: #e8e8e8;
+      height: -webkit-fill-available;
+      top: 0;
+      border-bottom-left-radius: 7px;
+      border-top-left-radius: 7px;
+    }
+    & .animalname {
+      z-index: 1;
+    }
+    & .circlecheck {
+      height: 25px;
+      z-index: 1;
+      margin-left: 15px;
+    }
+    & .divitem {
+      display: flex;
+    }
+  }
+`;
 
 export default function Poll({ qandas }: Props) {
   console.log('questions and answers: ', qandas);
-  return <PollWrapper>The Poll implementation goes here</PollWrapper>;
+  const [total_votes, setTotalvotes] = useState(0);
+  const [total_click, setTotalclick] = useState(0);
+  const [max_index, setMaxindex] = useState(0);
+  const [clicked, setClicked] = useState({ parent: -1, children: -1 });
+  const [activeQuestion, setActiveQuestion] = useState(0);
+  const handleAnswerClick = (i: any, index: any) => {
+    setTotalclick(total_click + 1);
+    setClicked({ parent: i, children: index });
+  };
+
+  const randomQuestion = () => {
+    const len = qandas.questions.length;
+    var randomnum = Math.floor(Math.random() * len);
+    var totalval = 0;
+    var maxval = 0;
+
+    setActiveQuestion(randomnum);
+
+    qandas.questions[randomnum].answers.map((answer, index) => {
+      totalval += answer.votes;
+      if (maxval < answer.votes) {
+        maxval = answer.votes;
+        setMaxindex(index);
+      }
+    });
+    setTotalvotes(totalval);
+  };
+
+  useEffect(() => {
+    randomQuestion();
+  }, []);
+
+  return (
+    <PollWrapper>
+      <div key={activeQuestion}>
+        <h2>{qandas.questions[activeQuestion].question.text}</h2>
+        {qandas.questions[activeQuestion].answers.map((answer, index) => (
+          <div
+            className="answer"
+            onClick={() => handleAnswerClick(activeQuestion, index)}
+            key={index}
+          >
+            <div
+              className="portion"
+              style={{
+                width: Math.round((answer.votes / total_votes) * 100) + '%',
+                backgroundColor: max_index === index ? 'cyan' : '',
+              }}
+            ></div>
+            <div className="divitem">
+              <span
+                className="animalname"
+                style={{ fontWeight: max_index === index ? 'bold' : '' }}
+              >
+                {answer.text}
+              </span>
+              {clicked.children === index && (
+                <img
+                  src={require('../static/check-circle.svg')}
+                  className="circlecheck"
+                />
+              )}
+            </div>
+            <span style={{ fontWeight: max_index === index ? 'bold' : '' }}>
+              {Math.round((answer.votes / total_votes) * 100) + '%'}
+            </span>
+          </div>
+        ))}
+        <p>{total_click} votes</p>
+      </div>
+    </PollWrapper>
+  );
 }
